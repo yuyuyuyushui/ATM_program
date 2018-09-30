249fb8c086151f9c238e928b45d53457e9ce4fbb
Administrator <786313105@qq.com>
2018-09-30 17:06:54 +0800

    ss

--- atm/core/main.py
+++ atm/core/main.py
@@ -4,27 +4,27 @@ from conf import settings
 from .accounts import read_meassage
 def main_():
     df = {
-        1: "转账",
-        2: "还款",
-        3: "取款",
-        4: "查询",
-        5: "账单"
+        1:
+        2: ,
+        3: ,
+        4: ,
+        5:
     }
-    dict_opreation = {
-        1: transfer_accounts,
-        2: repayment,
-        3: withdraw_money,
-        4: query_money,
-        5: pay_check
-    }
+    dict_opreation = [
+        ("转账",transfer_accounts),
+    ("还款", repayment),
+    ("取款", withdraw_money),
+    ("查询", query_money),
+    (  "账单", pay_check)
+    ]
     while not settings.LOGGING_STATE['admin']:
         admin = input("请输入用户名》》")
         passed = input('请输入密码》》')
         date = read_meassage(admin)
         if date['password'] == passed:
             settings.LOGGING_STATE["admin"] = True
-            for i in df:
-                print(i, df[i])
+            for index,val in dict_opreation:
+                print(index, val[0])
             chioce_op = input('请输入你选择的操作》》').strip()
             if int(chioce_op) in dict_opreation:
                 dict_opreation[int(chioce_op)]()
