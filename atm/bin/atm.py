1c37fe820de5985172fbd24909f641c1999039c6
e <786313105@qq.com>
2018-10-05 11:27:54 +0800

    atm

--- atm/bin/atm.py
+++ atm/bin/atm.py
@@ -1,1 +1,8 @@
 #atm执行程序
+import os,sys
+dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(dir_path)
+print(sys.path)
+from core import main
+if __name__ == "__main__":
+    main.main_()
